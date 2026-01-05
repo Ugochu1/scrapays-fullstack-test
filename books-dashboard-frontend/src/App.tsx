03fb2480a66f95283525f10fb85476a891d1cbe0
Ugochu1 <olinyaugochukwu1@gmail.com>
2026-01-05 17:11:54 +0100

    refactor: cleanup App component

--- books-dashboard-frontend/src/App.tsx
+++ books-dashboard-frontend/src/App.tsx
@@ -1,36 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { useAuth0 } from "@auth0/auth0-react";
+import AppLoader from "./components/common/AppLoader";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  if (isLoading) {
+    return (
+      <AppLoader loaderText="Sign in" />
+    );
+  }
+
+  if (!isAuthenticated) {
+    loginWithRedirect();
+    return null;
+  }
+
+  return <div>I can view app now</div>;
 }
 
 export default App;
