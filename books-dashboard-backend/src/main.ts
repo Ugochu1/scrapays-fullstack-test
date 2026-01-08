d28e4641c8f28bc9e95f2779f9970b8934093f1d
Ugochu1 <olinyaugochukwu1@gmail.com>
2026-01-08 19:50:52 +0100

    refactor: add custom cors settings

--- books-dashboard-backend/src/main.ts
+++ books-dashboard-backend/src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // enable cors with default settings
+  app.enableCors({
+    origin: process.env.ALLOW_ORIGIN, // Allow only this origin (e.g., your frontend app)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }); // enable cors with default settings
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
