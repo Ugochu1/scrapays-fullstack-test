9a9af8770a818f07396e1e565f7d26ccb0cbd5b5
Ugochu1 <olinyaugochukwu1@gmail.com>
2026-01-09 04:53:53 +0100

    fix: fix seed migration

--- books-dashboard-backend/src/migrations/1767928783516-SeedBooks.ts
+++ books-dashboard-backend/src/migrations/1767928783516-SeedBooks.ts
@@ -5,19 +5,20 @@ export class SeedBooks1767928783516 implements MigrationInterface {
     await queryRunner.query(`
             INSERT INTO "books"(name, description)
             VALUES 
-            ('Beloved by Toni Morrison', 'A haunting and profound novel about slavery’s legacy, centered on Sethe, a former enslaved woman haunted by the ghost of her daughter. This Pulitzer Prize–winning work explores memory, trauma, identity, and the deep bonds of family.'),
-            ('The Four Agreements by Don Miguel Ruiz', 'A short but powerful spiritual/self‑help book that presents four simple principles—agreements you can make with yourself—to reduce suffering and find personal freedom. It blends ancient Toltec wisdom with practical guidance for life.'),
-            ('The Silent Patient by Alex Michaelides', 'A gripping psychological thriller about Alicia Berenson, a famous painter who inexplicably shoots her husband and then stops speaking. Psychotherapist Theo Faber becomes obsessed with uncovering her motive, leading to shocking twists and revelations.'),
-            ('Educated by Tara Westover', 'A powerful memoir about growing up in a strict and isolated family in rural Idaho, where formal schooling was limited. Tara’s journey from her upbringing to earning a PhD illustrates resilience and the transformative power of education.'),
-            ('The Kite Runner by Khaled Hosseini', 'A moving story of friendship and redemption set against Afghanistan’s turbulent history. It follows Amir, who must confront his past and seek forgiveness for a childhood betrayal.'),
-            ('Thinking, Fast and Slow by Daniel Kahneman', 'A deep dive into how the human mind works, contrasting intuitive “fast” thinking with more deliberate “slow” thinking. Combines psychology and economics to explain decision-making.'),
-            ('The Hobbit by J.R.R. Tolkien', 'A fantasy adventure about Bilbo Baggins, a humble hobbit who embarks on an epic quest. This prelude to The Lord of the Rings is filled with courage, magic, and unforgettable characters.'),
-            ('The Power of Habit by Charles Duhigg', 'A nonfiction book that explores the science of habits—how they form, how they influence our lives, and how we can change them. Useful for personal development and behavior change.'),
-            ('Pride and Prejudice by Jane Austen', 'A beloved romantic novel that centers on Elizabeth Bennet and her sharp wit, navigating issues of class, marriage, and societal expectations in 19th-century England.'),
-            ('The Alchemist by Paulo Coelho', 'A philosophical and inspiring tale about a young shepherd named Santiago who travels in search of treasure. The story focuses on following your dreams, listening to your heart, and discovering your personal destiny.'),
-            ('Sapiens: A Brief History of Humankind by Yuval Noah Harari', 'A sweeping narrative of our species’ history—from ancient ancestors to modern civilization. It challenges assumptions about culture, economy, religion, and human behavior.'),
-            ('1984 by George Orwell', 'A dystopian masterpiece that imagines a totalitarian regime where “Big Brother” watches everyone. The book examines surveillance, truth, freedom, and propaganda—still relevant in today’s world.'),
-            ('To Kill a Mockingbird by Harper Lee', 'A classic of modern American literature, this novel follows young Scout Finch in the racially charged South as her father, Atticus, defends a Black man falsely accused of a crime. It explores justice, morality, and innocence.')
+            ('beloved by toni morrison', 'a haunting and profound novel about slavery’s legacy, centered on sethe, a former enslaved woman haunted by the ghost of her daughter. this pulitzer prize–winning work explores memory, trauma, identity, and the deep bonds of family.'),
+            ('the four agreements by don miguel ruiz', 'a short but powerful spiritual/self‑help book that presents four simple principles—agreements you can make with yourself—to reduce suffering and find personal freedom. it blends ancient toltec wisdom with practical guidance for life.'),
+            ('the silent patient by alex michaelides', 'a gripping psychological thriller about alicia berenson, a famous painter who inexplicably shoots her husband and then stops speaking. psychotherapist theo faber becomes obsessed with uncovering her motive, leading to shocking twists and revelations.'),
+            ('educated by tara westover', 'a powerful memoir about growing up in a strict and isolated family in rural idaho, where formal schooling was limited. tara’s journey from her upbringing to earning a phd illustrates resilience and the transformative power of education.'),
+            ('the kite runner by khaled hosseini', 'a moving story of friendship and redemption set against afghanistan’s turbulent history. it follows amir, who must confront his past and seek forgiveness for a childhood betrayal.'),
+            ('thinking, fast and slow by daniel kahneman', 'a deep dive into how the human mind works, contrasting intuitive “fast” thinking with more deliberate “slow” thinking. combines psychology and economics to explain decision-making.'),
+            ('the hobbit by j.r.r. tolkien', 'a fantasy adventure about bilbo baggins, a humble hobbit who embarks on an epic quest. this prelude to the lord of the rings is filled with courage, magic, and unforgettable characters.'),
+            ('the power of habit by charles duhigg', 'a nonfiction book that explores the science of habits—how they form, how they influence our lives, and how we can change them. useful for personal development and behavior change.'),
+            ('pride and prejudice by jane austen', 'a beloved romantic novel that centers on elizabeth bennet and her sharp wit, navigating issues of class, marriage, and societal expectations in 19th-century england.'),
+            ('the alchemist by paulo coelho', 'a philosophical and inspiring tale about a young shepherd named santiago who travels in search of treasure. the story focuses on following your dreams, listening to your heart, and discovering your personal destiny.'),
+            ('sapiens: a brief history of humankind by yuval noah harari', 'a sweeping narrative of our species’ history—from ancient ancestors to modern civilization. it challenges assumptions about culture, economy, religion, and human behavior.'),
+            ('1984 by george orwell', 'a dystopian masterpiece that imagines a totalitarian regime where “big brother” watches everyone. the book examines surveillance, truth, freedom, and propaganda—still relevant in today’s world.'),
+            ('to kill a mockingbird by harper lee', 'a classic of modern american literature, this novel follows young scout finch in the racially charged south as her father, atticus, defends a black man falsely accused of a crime. it explores justice, morality, and innocence.')
+
             `);
   }
 
@@ -25,19 +26,19 @@ export class SeedBooks1767928783516 implements MigrationInterface {
     await queryRunner.query(`
       DELETE FROM "books" WHERE "name" IN 
       (
-        'Beloved by Toni Morrison', 
-        'The Four Agreements by Don Miguel Ruiz',
-        'The Silent Patient by Alex Michaelides',
-        'Educated by Tara Westover',
-        'The Kite Runner by Khaled Hosseini',
-        'Thinking, Fast and Slow by Daniel Kahneman',
-        'The Hobbit by J.R.R. Tolkien',
-        'The Power of Habit by Charles Duhigg',
-        'Pride and Prejudice by Jane Austen',
-        'The Alchemist by Paulo Coelho',
-        'Sapiens: A Brief History of Humankind by Yuval Noah Harari',
-        '1984 by George Orwell',
-        'To Kill a Mockingbird by Harper Lee'
+        'beloved by toni morrison',
+        'the four agreements by don miguel ruiz',
+        'the silent patient by alex michaelides',
+        'educated by tara westover',
+        'the kite runner by khaled hosseini',
+        'thinking, fast and slow by daniel kahneman',
+        'the hobbit by j.r.r. tolkien',
+        'the power of habit by charles duhigg',
+        'pride and prejudice by jane austen',
+        'the alchemist by paulo coelho',
+        'sapiens: a brief history of humankind by yuval noah harari',
+        '1984 by george orwell',
+        'to kill a mockingbird by harper lee'
     )
     `);
   }
